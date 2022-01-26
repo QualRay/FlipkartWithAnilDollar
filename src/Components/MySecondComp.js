ddd47615a4fceb5b9e46ebf25d4ae4e7a54ef6ce
Ray Rana <shree3ray@gmail.com>
2022-01-25 17:54:32 -0700

    increment update

--- src/Components/MySecondComp.js
+++ src/Components/MySecondComp.js
@@ -3,12 +3,12 @@ import React, { useState } from 'react';
 function MySecondComp() {
 
     // 1  variable --> Function --------> Initial Value ---> Optional
-    const [salary, setSalary] = useState(40000)
+    const [salary, setSalary] = useState(50000)
 
     //2 Create Function -------- Optional
 
     let incr = () => {
-        setSalary(salary + 10000)
+        setSalary(salary + 20000)
     }
     
     /*3 { expressions }  and { () => {} } is known as annonymous callback -----> Mandatory */ 
