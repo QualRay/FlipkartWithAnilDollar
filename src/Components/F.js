66f45fe356ebbfa14882705f790e2bdb37186793
Ray Rana <shree3ray@gmail.com>
2022-02-06 00:20:02 -0700

    Added two way binding and API hit on F.js

--- src/Components/F.js
+++ src/Components/F.js
@@ -1,16 +1,47 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import swal from 'sweetalert';
 
 function F() {
 
     //set state from
-    // hook variable ---> variablename, functionName --> initial data
+    // hook variable ---> variablename, setFunctionName --> initial data
     const [studentName, setStudentName] = useState('');
 
     //funtion with fat arrow notation
 
     let myFunction = () =>{
         console.log( studentName );
+        // Call API Here
+        // This is also called promise chain
+        //fetch().then().catch().finally();
+
+        var data = {
+                "data": {
+                  "Name": studentName
+                }
+              }
+        
+
+        fetch('http://localhost:1337/api/students', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'accept': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }).then((d)=>{
+            console.log(d.status);
+            if(d.status === 200){
+                swal("Good Job!","Data created successfully!","Success");
+            }
+        }).catch((e)=>{}).finally((all)=>{});
     }
+
+    //useHooks -->
+    // useEffect(<function>, <dependency>) formula fat arrow annonymous function ,dependency on empty array []
+
+    useEffect( () => {} , []);
+    
   
     return (
         <>
